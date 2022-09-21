40813cd2b9ea8ccac49b76491aff04e7b9edd6b3
HeatherH <hherries@yahoo.com>
2022-09-21 11:49:57 -0600

    created general navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-import React from 'react'
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import './Navbar.css'
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import {FaRegTimesCircle} from 'react-icons/fa'
+import {BsFillHouseFill} from 'react-icons/bs'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='container'></div>
+        <h1><span><BsFillHouseFill />Real</span>Estate</h1>
+        <button className='btn'>Sign In</button>
+        <ul className='nav-menu'>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>Search</a></li>
+          <li><a href='#'>About</a></li>
+          <li><a href='#'>Contact</a></li>
+        </ul>
+        <div className='hamburger'>
+          <HiOutlineMenuAlt4 />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
