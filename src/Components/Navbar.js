7c0dc5963ed7b9d053ffc5a5272e835376ba1f67
HeatherH <hherries@yahoo.com>
2022-09-21 15:04:57 -0600

    Finished fuctionality of navbar for smaller screens so that when you click the hamburger menu it will bring up a transition to show menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,19 +5,23 @@ import {FaRegTimesCircle} from 'react-icons/fa'
 import {BsFillHouseFill} from 'react-icons/bs'
 
 function Navbar() {
+  const[click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+
   return (
     <div className='navbar'>
       <div className='container'>
         <h1><span><BsFillHouseFill />Real</span>Estate</h1>
         <button className='btn'>Sign In</button>
-        <ul className='nav-menu'>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li><a href='#'>Home</a></li>
           <li><a href='#'>Search</a></li>
           <li><a href='#'>About</a></li>
           <li><a href='#'>Contact</a></li>
         </ul>
-        <div className='hamburger'>
-          <HiOutlineMenuAlt4 className='icon'/>
+        <div className='hamburger' onClick={handleClick}>
+          {click ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon'/>)}
+          
         </div>
       </div>
     </div>
