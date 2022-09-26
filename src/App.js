9fb81ec0a1e812a8e88d85917ed96914c0e59298
HeatherH <hherries@yahoo.com>
2022-09-26 16:45:05 -0600

    Finished creating react for the featured section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero'
 import Best from './Components/Best/Best'
+import Featured from './Components/Featured/Featured'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Best />
+      <Featured />
     </div>
   );
 }
---
+++ src/Components/Featured/Featured.js
@@ -0,0 +1,106 @@
+
+import React from 'react'
+import './Featured.css'
+import House1 from '../../Assets/re2.jpg'
+import House2 from '../../Assets/re2.jpg'
+import House3 from '../../Assets/re2.jpg'
+import House4 from '../../Assets/re2.jpg'
+import House5 from '../../Assets/re2.jpg'
+
+import House6 from '../../Assets/re2.jpg'
+import House7 from '../../Assets/re2.jpg'
+import House8 from '../../Assets/re2.jpg'
+import House9 from '../../Assets/re2.jpg'
+import House10 from '../../Assets/re2.jpg'
+
+const Featured = () => {
+    return (
+        <div className='featured'>
+            <h1 className='featured-text'>Top Featured Listings</h1>
+            <p className='featured-text'>Selected listings by City, State, & Zip based on views.</p>
+            <div className='container'>
+                <img className='span-3 image-grid-row-2' src={House1} alt='' />
+                <img src={House2} alt='' />
+                <img src={House3} alt='' />
+                <img src={House4} alt='' />
+                <img src={House5} alt='' />
+                <div className='span-3 img-details'>
+                    <div className='top'>
+                        <h2>123 Acme St. Dallas, TX</h2>
+                        <p>House for Sale</p>
+                        <p className='price'>$2,677,000</p>
+                    </div>
+                    <div className='info-grid'>
+                        <div>
+                            <div className='info'>
+                                <p className='bold'>Bedrooms:</p><p>5</p>
+                            </div>
+                            <div className='info'>
+                                <p className='bold'>Bathrooms:</p><p>7</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='info'>
+                                <p className='bold'>Square Feet:</p><p>8,138</p>
+                            </div>
+                            <div className='info'>
+                                <p className='bold'>Est Payment:</p><p>$14,797/mo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='span-2 right-img-details'>
+                    <p>A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility, and built-in sauna. </p>
+                    <button className='btn'>View Listing</button>
+                </div>
+            </div>
+
+        {/* Section section */}
+        <div className='container'>
+                <img className='order-2' src={House6} alt='' />
+                <img className='order-3' src={House7} alt='' />
+
+                <img className='span-3 image-grid-row-2 order-1' src={House8} alt='' />
+
+
+                <img className='order-4' src={House9} alt='' />
+                <img className='order-5' src={House10} alt='' />
+
+                <div className='span-2 right-img-details order-7'>
+                    <p>A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility, and built-in sauna. </p>
+                    <button className='btn'>View Listing</button>
+                </div>
+
+
+                <div className='span-3 img-details order-6'>
+                    <div className='top'>
+                        <h2>123 Acme St. Dallas, TX</h2>
+                        <p>House for Sale</p>
+                        <p className='price'>$2,677,000</p>
+                    </div>
+                    <div className='info-grid'>
+                        <div>
+                            <div className='info'>
+                                <p className='bold'>Bedrooms:</p><p>5</p>
+                            </div>
+                            <div className='info'>
+                                <p className='bold'>Bathrooms:</p><p>7</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='info'>
+                                <p className='bold'>Square Feet:</p><p>8,138</p>
+                            </div>
+                            <div className='info'>
+                                <p className='bold'>Est Payment:</p><p>$14,797/mo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Featured
